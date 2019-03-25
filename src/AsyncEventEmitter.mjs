5c1c184d0ea492ac3a298b80f5b63fa81eeb9996
Kevin Thomas <hamgom95@gmail.com>
2019-03-25 01:17:17 +0100

    add seperate emit method for parallel handler executing

--- src/AsyncEventEmitter.mjs
+++ src/AsyncEventEmitter.mjs
@@ -4,17 +4,26 @@ import EventEmitter from "events";
  * EventEmitter with async emit method.
  */
 export class AsyncEventEmitter extends EventEmitter {
-    async emit(type, ...args) {
+    async _emit(type, args, parallel=false) {
         const handler = this._events[type];
         if (handler === undefined) return false;
 
         if (typeof handler[Symbol.iterator] === 'function') { // multiple handlers
-            const promises = [...handler].map(listener => Reflect.apply(listener, this, args));
-            await Promise.all(promises);
+            if (parallel) {
+                const promises = [...handler].map(listener => Reflect.apply(listener, this, args));
+                await Promise.all(promises);
+            } else { // one after another
+                for (const listener of handler) {
+                    await Reflect.apply(listener, this, args);
+                }
+            }
         } else { // one handler
             await Reflect.apply(handler, this, args);
         }
         return true;
     }
+
+    async emit(type, ...args) { return this._emit(type, args, false); }
+    async emitParallel(type, ...args) { return this._emit(type, args, true); }
 }
 
